96651d42400f503f933c9e21c17eda3e3a8c3817
jjxie <8jjxie@gmail.com>
2018-04-06 14:42:17 +0200

    A simple light sensor example

---
+++ README.md
@@ -0,0 +1,6 @@
+# sensor-example
+
+This is the source code for a simple light sensor example based on socket.io framework(https://github.com/socketio/chat-example). 
+
+This example can get data from three light sensors and apply collected data to control the light (RGB) of another light sensor.
+
---
+++ index.js
@@ -0,0 +1,61 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+var connectedIDArray = [];
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	console.log(socket.id);
+	connectedIDArray.push(socket.id);
+	console.log("Connected array length: " + connectedIDArray.length); 
+
+	// Send message
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		// send to all
+		io.emit('chat message', socket.id + " : " + msg);
+		// send to others except the sender
+		// socket.broadcast.emit('chat message', msg);
+	});
+
+	// Send sensor data
+	socket.on('sensor data red', function(sensorData){
+		io.emit('sensor data red',  sensorData);
+	});
+
+	// Send sensor data 2
+	socket.on('sensor data green', function(sensorData){
+		io.emit('sensor data green',  sensorData);
+	});
+
+	// Send sensor data 3
+	socket.on('sensor data blue', function(sensorData){
+		io.emit('sensor data blue',  sensorData);
+	});
+
+	// Online event
+	io.emit('online', socket.id);
+
+	// Disconnect	
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+		console.log(socket.id);
+		var index = connectedIDArray.indexOf(socket.id); 
+		if(index > -1){	
+			connectedIDArray.splice(index, 1);
+		}
+		console.log("Connected array length: " + connectedIDArray.length); 
+		// Offline event
+		io.emit('offline', socket.id);
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
